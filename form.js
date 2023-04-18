976bb17cc74e67ef9d4f5755bc52649d3fbdc647
Shubham Patil <85643395+heyshubhampatil@users.noreply.github.com>
2023-04-18 21:50:08 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,13 @@
+function SendMail(){
+    
+    var params={
+        from_name : document.getElementById("fname").value,
+        email_id :document.getElementById("email").value,
+        number : document.getElementById("number").value,
+        message : document.getElementById("Description").value
+    };
+    emailjs.send("service_lrun8kd" , "template_wu1xg48" , params).then(function (res){
+        alert("Your Mail Has Been Sent!");
+    })
+    
+}
